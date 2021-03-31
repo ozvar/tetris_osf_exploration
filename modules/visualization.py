b4b46c18bbd639ab83f9d916ce448a5e88e584ae
ozvar <ov525@york.ac.uk>
2021-03-31 14:43:07 +0000

    updated PCA and plotted component trajectories over episodes

--- modules/visualization.py
+++ modules/visualization.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import seaborn as sns
@@ -14,13 +15,15 @@ sns.set_theme(
     style='whitegrid',
     palette=['#c44e52', '#8c8c8c', '#937860', '#ccb974', '#4c72b0', '#dd8452'],  # reordered version of seaborn 'deep' palette
     font_scale=1.5)
+matplotlib.rcParams['figure.dpi'] = 200
 
-def scree_plot(eigenvalues):
+def scree_plot(eigenvalues, xlabel=None, ylabel=None, hline=True):
     """Generate line plot of the eigenvalues obtained from factor analysis"""
     x_values = np.arange(1, len(eigenvalues)+1)
     plt.scatter(x_values, eigenvalues)
     plt.plot(x_values, eigenvalues)
-    plt.xlabel('Factor')
-    plt.ylabel('Eigenvalue')
-    plt.axhline(y=1, linestyle='--')
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    if hline is True:
+        plt.axhline(y=1, linestyle='--')
     plt.show()
