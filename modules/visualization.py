1b0afbe34a978314f5e09fccad4671d6dc7f6f24
ozvar <ov525@york.ac.uk>
2023-01-29 17:27:20 +0000

    added vline to scree plot at inflection point and updated to svg

--- modules/visualization.py
+++ modules/visualization.py
@@ -22,12 +22,19 @@ def sns_styleset():
     sns.set_theme(context='paper',
                   style='whitegrid',
                   # palette='deep',
-                  palette=['#c44e52', '#8c8c8c', '#937860', '#ccb974', '#4c72b0', '#dd8452'],
+                  palette=['#c44e52',
+                           '#8c8c8c',
+                           '#937860',
+                           '#ccb974',
+                           '#4c72b0',
+                           '#dd8452'],
                   font='Arial')
     matplotlib.rcParams['figure.dpi']        = 300
     matplotlib.rcParams['axes.linewidth']    = 1
     matplotlib.rcParams['grid.color']        = '.8'
     matplotlib.rcParams['axes.edgecolor']    = '.15'
+    mpl.rcParams['axes.spines.right']        = False
+    mpl.rcParams['axes.spines.top']          = False
     matplotlib.rcParams['xtick.bottom']      = True
     matplotlib.rcParams['ytick.left']        = True
     matplotlib.rcParams['xtick.major.width'] = 1
@@ -36,27 +43,37 @@ def sns_styleset():
     matplotlib.rcParams['ytick.color']       = '.15'
     matplotlib.rcParams['xtick.major.size']  = 3
     matplotlib.rcParams['ytick.major.size']  = 3
-    matplotlib.rcParams['font.size']         = 11
-    matplotlib.rcParams['axes.titlesize']    = 11
-    matplotlib.rcParams['axes.labelsize']    = 10
-    matplotlib.rcParams['legend.fontsize']   = 10
+    matplotlib.rcParams['font.size']         = 14
+    matplotlib.rcParams['axes.titlesize']    = 14
+    matplotlib.rcParams['axes.labelsize']    = 13
+    matplotlib.rcParams['legend.fontsize']   = 14
     matplotlib.rcParams['legend.frameon']    = False
-    matplotlib.rcParams['xtick.labelsize']   = 10
-    matplotlib.rcParams['ytick.labelsize']   = 10
+    matplotlib.rcParams['xtick.labelsize']   = 13
+    matplotlib.rcParams['ytick.labelsize']   = 13
 
 
-def scree_plot(eigenvalues, xlabel=None, ylabel=None, hline=True):
+def scree_plot(eigenvalues, xlabel=None, ylabel=None, vline=True, fig_dir=None):
     """Generate line plot of the eigenvalues obtained from factor analysis"""
     x_values = np.arange(1, len(eigenvalues)+1)
     plt.scatter(x_values, eigenvalues)
     plt.plot(x_values, eigenvalues)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
-    if hline is True:
-        plt.axhline(y=1, linestyle='--')
+    if vline is True:
+        plt.vlines(
+                4,
+                plt.ylim()[0],
+                plt.ylim()[1],
+                color='red',
+                linestyle='--')
+
     plt.show()
+    if fig_dir != None:
+        fig_name = 'PCA_scree_plot.svg'
+        plt.savefig(os.path.join(fig_dir, fig_name))
+    plt.close()
     
-    
+
 def error_line(df, var, n, ax=None, xlabel=None, ylabel=None, label=None):
     """Plot the mean of variable 'var' at each match for all account_ids over 'n' matches, with shaded regions indicating SEM"""
     means = compute_means(df, var, n)
